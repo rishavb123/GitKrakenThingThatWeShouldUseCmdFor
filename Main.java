c247d6204e159b7391c0340e80046d82e47fc05b
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-05-29 10:10:07 -0400

    Runner and interface for perfect square functionality created

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Inter inter = (int num) -> Math.sqrt(num) == Math.round(Math.sqrt(num));
+        System.out.println(inter.perfectSquare(5));
     }
+}
+
+interface Inter {
+
+    public boolean perfectSquare(int num);
+
 }
