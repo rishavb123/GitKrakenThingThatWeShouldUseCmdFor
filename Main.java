1bfc9604da348c3a155a2e3ca637d12c3c60fc33
Karthik Sankar <kihtrak@kihtrak.com>
2019-05-29 10:22:42 -0400

    Runner and interface for printing quotient functionality created

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Inter2 hi = (float a, float b) -> System.out.printf("%.3f / %.3f = %.3f", a, b, a / b);
+		hi.methodOne(1, 3);
     }
+}
+
+
+interface Inter2 {
+	void methodOne(float a, float b);
 }
